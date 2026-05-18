693ee3ffe78ce99b3e929a10a82536c5676d613f
Miguel Ángel Galdón Dios <galdondios@gmail.com>
2026-05-19 00:31:57 +0200

    Listo para entrega

--- server/app.js
+++ server/app.js
@@ -24,8 +24,8 @@ if (!isProxmox) {
   db.init({
     host: '127.0.0.1',
     port: 3306,
-    user: 'super',
-    password: '1234',
+    user: 'root',
+    password: 'P@ssw0rd',
     database: 'sakila'
   });
 }
@@ -65,6 +65,8 @@ app.get('/', async (req, res) => {
       GROUP BY f.film_id LIMIT 5`);
     const categoriesRows = await db.query('SELECT name as nom FROM category LIMIT 5');
     
+    // Transformar les dades a JSON (per les plantilles .hbs)
+    // Cal informar de les columnes i els seus tipus
     const moviesJson = db.table_to_json(moviesRows, { 
       nom: 'string', 
       any: 'number', 
@@ -74,16 +76,19 @@ app.get('/', async (req, res) => {
       nom: 'string' 
     });
 
+    // Llegir l'arxiu .json amb dades comunes per a totes les pàgines
     const commonData = JSON.parse(
       fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8')
     );
 
+    // Construir l'objecte de dades per a la plantilla
     const data = {
       movies: moviesJson,
       categories: categoriesJson,
       common: commonData
     };
 
+    // Renderitzar la plantilla amb les dades
     res.render('index', data);
   } catch (err) {
     console.error(err);
@@ -93,14 +98,18 @@ app.get('/', async (req, res) => {
 
 app.get('/movies', async (req, res) => {
   try {
+
+    // Obtenir les dades de la base de dades
     const moviesRows = await db.query(`
-      SELECT f.film_id, f.title, f.description, f.release_year, GROUP_CONCAT(a.first_name, ' ', a.last_name SEPARATOR ', ') AS actors
+      SELECT f.film_id, f.title, f.description, f.release_year,
+        GROUP_CONCAT(a.first_name, ' ', a.last_name SEPARATOR ', ') AS actors
       FROM film f
       LEFT JOIN film_actor fa ON f.film_id = fa.film_id
       LEFT JOIN actor a ON fa.actor_id = a.actor_id
       GROUP BY f.film_id
       LIMIT 15`);
 
+    // Transformar les dades a JSON (per les plantilles .hbs)
     const moviesJson = db.table_to_json(moviesRows, {
       film_id: 'number',
       title: 'string',
@@ -109,15 +118,18 @@ app.get('/movies', async (req, res) => {
       actors: 'string'
     });
 
+    // Llegir l'arxiu .json amb dades comunes per a totes les pàgines
     const commonData = JSON.parse(
       fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8')
     );
 
+    // Construir l'objecte de dades per a la plantilla
     const data = {
       movies: moviesJson,
       common: commonData
     };
 
+    // Renderitzar la plantilla amb les dades
     res.render('movies', data);
   } catch (err) {
     console.error(err);
@@ -127,8 +139,11 @@ app.get('/movies', async (req, res) => {
 
 app.get('/customers', async (req, res) => {
   try {
+
+    // Obtenir les dades de la base de dades
     const customerRows = await db.query(`
-      SELECT c.first_name, c.last_name, c.email, GROUP_CONCAT(f.title SEPARATOR ' || ') as lloguers
+      SELECT c.first_name, c.last_name, c.email,
+        GROUP_CONCAT(f.title SEPARATOR ' || ') as lloguers
       FROM customer c
       LEFT JOIN rental r ON c.customer_id = r.customer_id
       LEFT JOIN inventory i ON r.inventory_id = i.inventory_id
@@ -136,22 +151,29 @@ app.get('/customers', async (req, res) => {
       GROUP BY c.customer_id
       LIMIT 25`);
 
+    // Transformar les dades a JSON (per les plantilles .hbs)
     const customersJson = db.table_to_json(customerRows, {
       first_name: 'string',
       last_name: 'string',
       email: 'string',
       lloguers: 'string'
-    });
+    }).map(c => ({
+      ...c,
+      lloguers: c.lloguers ? c.lloguers.split(' || ').slice(0, 5).join(' || ') : null
+    }));
 
+    // Llegir l'arxiu .json amb dades comunes per a totes les pàgines
     const commonData = JSON.parse(
       fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8')
     );
 
+    // Construir l'objecte de dades per a la plantilla
     const data = {
       customers: customersJson,
       common: commonData
     };
 
+    // Renderitzar la plantilla amb les dades
     res.render('customers', data);
   } catch (err) {
     console.error(err);
@@ -159,21 +181,21 @@ app.get('/customers', async (req, res) => {
   }
 });
 
-// fitxa d'una pelicula
-app.get('/movie', async (req, res) => {
+app.get('/movie/:id', async (req, res) => {
   try {
-    const filmId = parseInt(req.query.id, 10)
-
+    const id = req.params.id;
+ 
     const movieRows = await db.query(`
-      SELECT f.film_id, f.title, f.description, f.release_year, f.length, f.rating, f.rental_rate, l.name as language,
-      GROUP_CONCAT(a.first_name, ' ', a.last_name SEPARATOR ', ') AS actors
+      SELECT f.film_id, f.title, f.description, f.release_year, f.length, f.rating,
+        f.language_id, l.name as language,
+        GROUP_CONCAT(a.first_name, ' ', a.last_name SEPARATOR ', ') AS actors
       FROM film f
       LEFT JOIN language l ON f.language_id = l.language_id
       LEFT JOIN film_actor fa ON f.film_id = fa.film_id
       LEFT JOIN actor a ON fa.actor_id = a.actor_id
-      WHERE f.film_id = ${filmId}
-      GROUP BY f.film_id`)
-
+      WHERE f.film_id = ${id}
+      GROUP BY f.film_id`);
+ 
     const movieJson = db.table_to_json(movieRows, {
       film_id: 'number',
       title: 'string',
@@ -181,49 +203,43 @@ app.get('/movie', async (req, res) => {
       release_year: 'number',
       length: 'number',
       rating: 'string',
-      rental_rate: 'number',
+      language_id: 'number',
       language: 'string',
       actors: 'string'
-    })
-
-    const commonData = JSON.parse(
-      fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8')
-    )
-
-    res.render('movie', { movie: movieJson[0], common: commonData })
+    });
+ 
+    const commonData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8'));
+ 
+    res.render('movie', { movie: movieJson[0], common: commonData });
   } catch (err) {
-    console.error(err)
-    res.status(500).send('Error consultant la base de dades')
+    console.error(err);
+    res.status(500).send('Error consultant la base de dades');
   }
-})
+});
 
-// pagina per afegir pelicula
 app.get('/movieAdd', async (req, res) => {
   try {
-    const languageRows = await db.query('SELECT language_id, name FROM language')
-    const languagesJson = db.table_to_json(languageRows, { language_id: 'number', name: 'string' })
-
-    const commonData = JSON.parse(
-      fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8')
-    )
-
-    res.render('movieAdd', { languages: languagesJson, common: commonData })
+    const languageRows = await db.query('SELECT language_id, name FROM language');
+    const languagesJson = db.table_to_json(languageRows, { language_id: 'number', name: 'string' });
+ 
+    const commonData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8'));
+ 
+    res.render('movieAdd', { languages: languagesJson, common: commonData });
   } catch (err) {
-    console.error(err)
-    res.status(500).send('Error consultant la base de dades')
+    console.error(err);
+    res.status(500).send('Error consultant la base de dades');
   }
-})
+});
 
-// pagina per editar pelicula
-app.get('/movieEdit', async (req, res) => {
+app.get('/movieEdit/:id', async (req, res) => {
   try {
-    const filmId = parseInt(req.query.id, 10)
-
+    const id = req.params.id;
+ 
     const movieRows = await db.query(`
-      SELECT film_id, title, description, release_year, length, rating, rental_rate, rental_duration, replacement_cost, language_id
+      SELECT film_id, title, description, release_year, length, rating, language_id
       FROM film
-      WHERE film_id = ${filmId}`)
-
+      WHERE film_id = ${id}`);
+ 
     const movieJson = db.table_to_json(movieRows, {
       film_id: 'number',
       title: 'string',
@@ -231,84 +247,71 @@ app.get('/movieEdit', async (req, res) => {
       release_year: 'number',
       length: 'number',
       rating: 'string',
-      rental_rate: 'number',
-      rental_duration: 'number',
-      replacement_cost: 'number',
       language_id: 'number'
-    })
-
-    const languageRows = await db.query('SELECT language_id, name FROM language')
-    const languagesJson = db.table_to_json(languageRows, { language_id: 'number', name: 'string' })
-
-    const commonData = JSON.parse(
-      fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8')
-    )
-
-    res.render('movieEdit', { movie: movieJson[0], languages: languagesJson, common: commonData })
+    });
+ 
+    const languageRows = await db.query('SELECT language_id, name FROM language');
+    const languagesJson = db.table_to_json(languageRows, { language_id: 'number', name: 'string' });
+ 
+    const commonData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'common.json'), 'utf8'));
+ 
+    res.render('movieEdit', { movie: movieJson[0], languages: languagesJson, common: commonData });
   } catch (err) {
-    console.error(err)
-    res.status(500).send('Error consultant la base de dades')
+    console.error(err);
+    res.status(500).send('Error consultant la base de dades');
   }
-})
+});
 
-// afegir pelicula
 app.post('/afegirPeli', async (req, res) => {
   try {
-    const title = req.body.title
-    const description = req.body.description
-    const release_year = req.body.release_year
-    const language_id = req.body.language_id
-
+    const { title, description, release_year, language_id, length, rating } = req.body;
+ 
     await db.query(`
-      INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, replacement_cost)
-      VALUES ("${title}", "${description}", ${release_year}, ${language_id}, 3, 4.99, 19.99)`)
-
-    res.redirect('/movies')
+      INSERT INTO film (title, description, release_year, language_id, length, rating, rental_duration, rental_rate, replacement_cost)
+      VALUES ('${title}', '${description}', ${release_year}, ${language_id}, ${length}, '${rating}', 3, 4.99, 19.99)`);
+ 
+    res.redirect('/movies');
   } catch (err) {
-    console.error(err)
-    res.status(500).send('Error afegint la pelicula')
+    console.error(err);
+    res.status(500).send('Error afegint la pel·lícula');
   }
-})
+});
 
-// editar pelicula
 app.post('/editarPeli', async (req, res) => {
   try {
-    const film_id = req.body.film_id
-    const title = req.body.title
-    const description = req.body.description
-    const release_year = req.body.release_year
-    const language_id = req.body.language_id
-    const rating = req.body.rating
-
+    const { film_id, title, description, release_year, language_id, length, rating } = req.body;
+ 
     await db.query(`
-      UPDATE film SET
-        title = "${title}",
-        description = "${description}",
-        release_year = ${release_year},
-        language_id = ${language_id},
-        rating = "${rating}"
-      WHERE film_id = ${film_id}`)
-
-    res.redirect('/movie?id=' + film_id)
+      UPDATE film
+      SET title = '${title}',
+          description = '${description}',
+          release_year = ${release_year},
+          language_id = ${language_id},
+          length = ${length},
+          rating = '${rating}'
+      WHERE film_id = ${film_id}`);
+ 
+    res.redirect('/movie/' + film_id);
   } catch (err) {
-    console.error(err)
-    res.status(500).send('Error editant la pelicula')
+    console.error(err);
+    res.status(500).send('Error editant la pel·lícula');
   }
-})
+});
 
-// esborrar pelicula
 app.post('/esborrarPeli', async (req, res) => {
   try {
-    const film_id = req.body.film_id
-
-    await db.query(`DELETE FROM film WHERE film_id = ${film_id}`)
-
-    res.redirect('/movies')
+    const { film_id } = req.body;
+ 
+    await db.query(`DELETE FROM film_actor WHERE film_id = ${film_id}`);
+    await db.query(`DELETE FROM film_category WHERE film_id = ${film_id}`);
+    await db.query(`DELETE FROM film WHERE film_id = ${film_id}`);
+ 
+    res.redirect('/movies');
   } catch (err) {
-    console.error(err)
-    res.status(500).send('Error esborrant la pelicula')
+    console.error(err);
+    res.status(500).send('Error esborrant la pel·lícula');
   }
-})
+});
 
 // Start server
 const httpServer = app.listen(port, () => {
